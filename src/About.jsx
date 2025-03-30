17e0081a8307c730fc14903e90e58160bc79a71b
pratik <pratikgurung@gmail.com>
2025-03-30 09:20:03 +0545

    askdjf

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import Card from './components/Card'
 
 export const About = () => {
+
+  const test=()=>{
+   alert(1+1)
+  };
   return (
-    <div>About</div>
+    <>
+
+      <h1>ABout </h1>
+
+      <div className='grid grid-cols-3 gap-8'>
+        <Card  description="test" abc={test} />
+        <Card title="abc" description="test" abc={test} />
+        <Card title="abc" description="test" abc={test} />
+      </div>
+
+    </>
   )
 }
